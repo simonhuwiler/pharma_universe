3f0fd5484d84f56374c7a9fff1e44dfd9485df29
simonhuwiler <webmaster@simonhuwiler.ch>
2021-08-25 22:03:50 +0200

    init

---
+++ client/src/App.svelte
@@ -0,0 +1,145 @@
+<script>
+	// import './style.css'
+  import { onMount } from 'svelte';
+  import * as THREE from 'three'
+  import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+  onMount(async () => {
+		/**
+		 * Base
+		 */
+		// Canvas
+		const canvas = document.querySelector('canvas.webgl')
+    console.log(canvas)
+
+		// Scene
+		const scene = new THREE.Scene()
+
+		// Object
+		// const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+    const r = 100
+    const teilkreise = 5
+    const n = 25
+
+    var planets = []
+    
+    
+    for(let i = 0; i < n; i++)
+    {
+      // Create Sphere
+      const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+      const geometry = new THREE.SphereGeometry( 8, 32, 16 );
+      const sphere = new THREE.Mesh( geometry, material );
+
+
+      let y = Math.floor(i / teilkreise) * (r / teilkreise) + (r / teilkreise) / 2
+      let r_new = Math.sqrt(2 * r * y - Math.pow(y, 2))
+
+      let angles = n / teilkreise
+
+      let angle = i % angles * (360 / angles);
+      let x = r_new * Math.cos(angle)
+      let z = r_new * Math.sin(angle)
+      
+      sphere.position.set(x, y, z)
+      planets.push(sphere)
+
+    }
+
+    console.log(planets)
+    planets.forEach(p => scene.add(p))
+
+		// const geometry = new THREE.BufferGeometry()
+
+		// const count = 500;
+		// const positionArray = new Float32Array(count * 3 * 3)
+
+		// for(let i = 0; i < count * 3 * 3; i++)
+		// {
+		// 	positionArray[i] = (Math.random() - 0.5) * 4
+		// }
+
+		// const positionsAttribute = new THREE.BufferAttribute(positionArray, 3)
+
+		// geometry.setAttribute('position', positionsAttribute)
+
+
+		// const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+		// const mesh = new THREE.Mesh(geometry, material)
+		// scene.add(mesh)
+
+		// Sizes
+		const sizes = {
+			width: window.innerWidth,
+			height: window.innerHeight
+		}
+
+		window.addEventListener('resize', () =>
+		{
+			// Update sizes
+			sizes.width = window.innerWidth
+			sizes.height = window.innerHeight
+
+			// Update camera
+			camera.aspect = sizes.width / sizes.height
+			camera.updateProjectionMatrix()
+
+			// Update renderer
+			renderer.setSize(sizes.width, sizes.height)
+			renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+		})
+
+		// Camera
+		const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
+		camera.position.z = 0
+		scene.add(camera)
+
+		// Controls
+		const controls = new OrbitControls(camera, canvas)
+		controls.enableDamping = true
+
+		// Renderer
+		const renderer = new THREE.WebGLRenderer({
+			canvas: canvas
+		})
+		renderer.setSize(sizes.width, sizes.height)
+		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+		// Animate
+		const clock = new THREE.Clock()
+
+		const tick = () =>
+		{
+			const elapsedTime = clock.getElapsedTime()
+
+			// Update controls
+			controls.update()
+
+			// Render
+			renderer.render(scene, camera)
+
+			// Call tick again on the next frame
+			window.requestAnimationFrame(tick)
+		}
+    
+		tick()
+	});
+
+</script>
+
+<main>
+	<canvas class="webgl"></canvas>
+</main>
+
+<style>
+	.webgl {
+		width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    outline: none;
+	}
+
+</style>
