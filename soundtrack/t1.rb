06f5fe54e0dffdd291ea8b60450fe56a78fc4f9b
Simon Huwiler <webmaster@simonhuwiler.ch>
2021-12-03 21:47:47 +0100

    wip

--- soundtrack/t1.rb
+++ soundtrack/t1.rb
@@ -1,8 +1,8 @@
 
 use_bpm 160
-
+=begin
 use_synth :dark_ambience
-play "C2", attack: 15, sustain: 250, amp: 0.5
+play "C2", attack: 15, sustain: 550, amp: 0.5
 
 sleep 30
 
@@ -23,94 +23,74 @@ with_synth :tb303 do
     sleep 4
   end
 end
-
+=end
 use_synth :hollow
-play chord(:f3, :minor), attack: 5, sustain: 200, release: 2
+play chord(:f3, :minor), attack: 5, sustain: 500, release: 2
+sleep 4
 
-with_synth :prophet do
-  with_fx :reverb, room: 1, amp: 0.1, amp_slide: 5 do |fx|
-    control fx, amp: 0.6
-    sleep 4
-    2.times do
-      
-      8.times do
-        play_pattern_timed [:f4, :gs4, :c5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
-        sleep 0.01
-      end
-      8.times do
-        play_pattern_timed [:as4, :cs5, :f5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
-        sleep 0.01
-      end
-      8.times do
-        play_pattern_timed [:f4, :gs4, :c5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
-        sleep 0.01
-      end
-      8.times do
-        play_pattern_timed [:c4, :e4, :g4], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
-        sleep 0.01
+define :background do |loops|
+  in_thread do
+    with_synth :prophet do
+      with_fx :reverb, room: 1, amp: 0.1, amp_slide: 5 do |fx|
+        control fx, amp: 0.6
+        loops.times do
+          
+          8.times do
+            play_pattern_timed [:f4, :gs4, :c5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
+            sleep 0.01
+          end
+          8.times do
+            play_pattern_timed [:as4, :cs5, :f5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
+            sleep 0.01
+          end
+          8.times do
+            play_pattern_timed [:f4, :gs4, :c5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
+            sleep 0.01
+          end
+          8.times do
+            play_pattern_timed [:c4, :e4, :g4], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
+            sleep 0.01
+          end
+        end
+        
+        #play_pattern_timed [:f5, :c5, :gs4, :c5, :gs4, :f4, :gs4, :f4, :c4, :f4], 0.125, release: 0.2, attack: 0.05, cutoff: rrand(30, 90), amp: 1
+        
       end
     end
     
-    #play_pattern_timed [:f5, :c5, :gs4, :c5, :gs4, :f4, :gs4, :f4, :c4, :f4], 0.125, release: 0.2, attack: 0.05, cutoff: rrand(30, 90), amp: 1
     
   end
-  
 end
 
-define :rythm_with_slight_bass do
+
+define :rythm_with_slight_bass do |loops|
   in_thread do
     with_synth :fm do
       with_fx :reverb do
-        2.times do
-          play_chord [:f1, :gs3, :c4, :f2, :f3], release: 0.2, attack: 0.05, amp: 2
-          sleep 1
-        end
-        sleep 8 - 2
-        2.times do
-          play_chord [:as1, :f1, :cs4, :as4, :f3], release: 0.2, attack: 0.05, amp: 2
-          sleep 1
-        end
-        sleep 8 - 2
-        2.times do
-          play_chord [:f1, :gs3, :c4, :f2, :f3], release: 0.2, attack: 0.05, amp: 2
-          sleep 1
-        end
-        sleep 8 - 2
-        2.times do
-          play_chord [:c1, :e2, :g2, :c3, :as2], release: 0.2, attack: 0.05, amp: 2
-          sleep 1
+        loops.times do
+          2.times do
+            play_chord [:f1, :gs3, :c4, :f2, :f3], release: 0.2, attack: 0.05, amp: 2
+            sleep 1
+          end
+          sleep 8 - 2
+          2.times do
+            play_chord [:as1, :f1, :cs4, :as4, :f3], release: 0.2, attack: 0.05, amp: 2
+            sleep 1
+          end
+          sleep 8 - 2
+          2.times do
+            play_chord [:f1, :gs3, :c4, :f2, :f3], release: 0.2, attack: 0.05, amp: 2
+            sleep 1
+          end
+          sleep 8 - 2
+          2.times do
+            play_chord [:c1, :e2, :g2, :c3, :as2], release: 0.2, attack: 0.05, amp: 2
+            sleep 1
+          end
+          sleep 8 - 2
         end
       end
     end
-  end
-  
-  
-  with_synth :prophet do
-    with_fx :reverb, room: 1, amp: 0.6, amp_slide: 5 do |fx|
-      2.times do
-        
-        8.times do
-          play_pattern_timed [:f4, :gs4, :c5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
-          sleep 0.01
-        end
-        8.times do
-          play_pattern_timed [:as4, :cs5, :f5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
-          sleep 0.01
-        end
-        8.times do
-          play_pattern_timed [:f4, :gs4, :c5], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
-          sleep 0.01
-        end
-        8.times do
-          play_pattern_timed [:c4, :e4, :g4], 0.33, release: 0.2, attack: 0.05, cutoff: rrand(60, 90), amp: 0.4
-          sleep 0.01
-        end
-      end
-      
-      #play_pattern_timed [:f5, :c5, :gs4, :c5, :gs4, :f4, :gs4, :f4, :c4, :f4], 0.125, release: 0.2, attack: 0.05, cutoff: rrand(30, 90), amp: 1
-      
-    end
-    
   end
 end
 
@@ -132,7 +112,12 @@ define :beep_beep do |n|
   end
 end
 
-rythm_with_slight_bass
+background 2
+sleep 64
+
+background 1
+rythm_with_slight_bass 1
+sleep 32
 
 with_fx :reverb, room: 1 do
   #live_loop :time do
@@ -153,7 +138,7 @@ end
 
 in_thread do
   2.times do
-    rythm_with_slight_bass
+    rythm_with_slight_bass 2
     16.times do
       4.times do
         sample :bd_haus
@@ -161,8 +146,8 @@ in_thread do
       end
     end
   end
-
-
+  
+  
 end
 
 sleep 32
@@ -177,25 +162,25 @@ in_thread do
           sleep 0.02
           play_pattern_timed [:gs4, :f4], 1, attack: 0,  release: 0.9, amp: 1
         end
-
+        
         2.times do
           play_pattern_timed [:as4, :cs5,:as4, :cs5, :as4, :c5, :cs5, :c5, :as4, :f4, :ds4, :cs4], 0.33, attack: 0, release: 0.4, amp: 1
           sleep 0.04
         end
-
+        
         2.times do
           play_pattern_timed [:f4, :gs4, :f4, :as4, :cs5, :g4], 0.33, attack: 0, release: 0.4, amp: 1
           sleep 0.02
           play_pattern_timed [:gs4, :f4], 1, attack: 0,  release: 0.9, amp: 1
         end
-
+        
         4.times do
-
+          
           play_pattern_timed [:f4, :ds4, :cs4, :ds4, :cs4, :c4], 0.33, attack: 0, release: 0.4, amp: 1
           sleep 0.02
         end
         #play :as3, release: 1
-
+        
       end
     end
   end
