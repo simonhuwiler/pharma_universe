8057f584fd355125edd24bc7afdf1c7fdb5882ed
simonhuwiler <webmaster@simonhuwiler.ch>
2021-12-06 09:49:54 +0100

    cleanup

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -32,10 +32,11 @@
   import en from './i18n/en.json'
   import de from './i18n/de.json'
 
-  // --- DEBUG
+  // --- DEBUG: This removes textures for planets and background sound and activates dat.gui
   const debug = false
   // ---
 
+  // ----------------------- Add i18n
   addMessages('en', en)
   addMessages('de', de)
 
@@ -46,16 +47,19 @@
 
   var loadingCounter = 0
   let activateControls = false
-  let showIntro = true
-  let showEasteregg = false
-  let showInstructions = false
   var animationArray = []
   var hideableObjects = []
   var timerHideableObjects
   let audioAmbient
   var controlAccelerator, controlMouse;
+  let huds = []
+  var activeAsteroid = null
 
-  // Stores
+  // ----------------------- Init and registerStores
+  let showIntro = true
+  let showEasteregg = false
+  let showInstructions = false  
+
   storeControlsEnabled.subscribe(value => activateControls = value);
   storeShowIntro.subscribe(value => {
     showIntro = value
@@ -72,11 +76,10 @@
   storeAnimationArray.subscribe(value => animationArray = value)
   storeShowEasteregg.subscribe(value => showEasteregg = value)
   
-  let huds = []
-  var activeAsteroid = null
 
-  // Add Gui
-  const gui = new dat.GUI({name: 'My GUI'});
+
+  // ----------------------- Add dat.gui
+  const gui = new dat.GUI({name: 'Debug'});
   var stats
   if(debug)
   {
@@ -86,12 +89,14 @@
   }
   if(!debug) gui.hide()
 
+  // ----------------------- Fetch data json
   loadingCounter++;
   fetch('./data.json')
     .then(response => response.json())
     .then(data => {
       const canvas = document.querySelector('canvas.webgl')
 
+      // Subscribe intro click on astesroid 6950
       storeShowStahle.subscribe(value => {
         if(value) 
         {
@@ -102,6 +107,7 @@
         }
       })
 
+      // On this object user is able to click (asteroids and planets)
       const groupClickable = new THREE.Group();
 
       // Scene
@@ -120,14 +126,14 @@
       // var m = new THREE.Mesh(tubeGeometry, new THREE.MeshBasicMaterial({color: 'red'}))
       // scene.add(m)
 
-      // Generate loaders
+      // Generate loaders depending on debug or production
       const loaders = []
       if(debug)
         settings.planetTexturesDebug.forEach(tx => loaders.push(textureLoader.load(`./textures/planets/${tx}`)))
       else
         settings.planetTextures.forEach(tx => loaders.push(textureLoader.load(`./textures/planets/${tx}`)))
 
-      // Load Special Planet
+      // Load the one special planet
       var materialPlanetLava = null
       if(!debug)
       {
@@ -140,10 +146,13 @@
         materialPlanetLava.aoMap = textureLoader.load(`./textures/lava/l00kFoivOUCl2OUtQVZVZQ_4K_AO.jpg`)
       }
       
+      // ----------------------- With nearby we detect nearest asteroids as well as which ones are far enough to hide
       const nearbyFactor = 100000000
       const nearbyAsteroids = new Nearby(data.solarsystem_r * 2, data.solarsystem_r * 2, data.solarsystem_r * 2, nearbyFactor);
       const nearbyPlanets = new Nearby(data.solarsystem_r * 2, data.solarsystem_r * 2, data.solarsystem_r * 2, data.solarsystem_r);
       const nearbyVisible = new Nearby(data.solarsystem_r * 2, data.solarsystem_r * 2, data.solarsystem_r * 2, nearbyFactor * (isMobile(window.navigator).any ? 1 : 3));
+
+      // ----------------------- Load planets based on data.json
       const planets = []
       loadingCounter++
       const texturePromis = Promise.all(loaders, (resolve, reject) => {
@@ -151,29 +160,22 @@
       }).then(textures => {
 
         // Textures Loaded. Create Planets
-        console.log("Load Planets")
 
         for(let i in data.planets)
         {
           let planet = data.planets[i]
 
           var materialPlanet;
-          // Create Material
-          if(planet.id === 40 && !debug)
-          {
-            // Lava planet
-            materialPlanet = materialPlanetLava
-          }
-          else
-          {
-            // materialPlanet = new THREE.MeshPhongMaterial({map: textures[0]});
-            materialPlanet = new THREE.MeshPhongMaterial({map: textures[i % textures.length]});     
-          }  
+
+          // Is Lava planet? Then other textures
+          if(planet.id === 40 && !debug) materialPlanet = materialPlanetLava
+          else materialPlanet = new THREE.MeshPhongMaterial({map: textures[i % textures.length]});     
 
           // Create Sphere
           const geometry = new THREE.SphereGeometry( planet.size, 64, 64 );
           const sphere = new THREE.Mesh( geometry, materialPlanet );
 
+          // Add user data for ray tracing
           sphere.userData.type = 'planet'
           sphere.userData.id = planet.id
           sphere.userData.name = planet.name
@@ -184,7 +186,7 @@
           planets.push(sphere)
           groupClickable.add(sphere)
 
-          // Add Nearby
+          // Add Nearby (for the hud)
           var box = nearbyPlanets.createBox(
             planet.x, planet.y, planet.z,
             planet.size, planet.size, planet.size
@@ -196,8 +198,9 @@
         loadingCounter++
       })
 
-      var color = '#edc99d';
-      var materialAsteroid = new THREE.MeshStandardMaterial({color:color, roughness: 0.8, metalness: 0.8});
+      // ----------------------- Create Asteroids based on data.json
+      const color = '#edc99d';
+      const materialAsteroid = new THREE.MeshStandardMaterial({color:color, roughness: 0.8, metalness: 0.8});
       materialAsteroid.flatShading = true
 
       let asteroids = []
@@ -221,9 +224,11 @@
         mesh.userData.id = p.id
 
         asteroids.push(mesh)
+
+        // Add only every second "moon" (asteroids with one recipient) to the hideableObjects. So we still see the asteroid beld around a planet
         if(p.type == 'moon') moonCount++
         if(p.type == 'asteroid' && p.size < 900000) hideableObjects.push(mesh)
-        if(p.type == 'moon' && moonCount % 3 == 0) hideableObjects.push(mesh)
+        if(p.type == 'moon' && moonCount % 2 == 0) hideableObjects.push(mesh)
 
         // Add Nearby
         var box = nearbyAsteroids.createBox(
@@ -237,13 +242,14 @@
       }
       loadingCounter = 0
 
+      // ----------------------- Add light
       const light = new THREE.PointLight( 0xffffff, 1 );
       const ambientLight = new THREE.AmbientLight( 0xffffff, 0.1 );
       light.position.y = data.solarsystem_r / 2
       scene.add( light );
       scene.add( ambientLight );
 
-      // Add Easteregg
+      // ----------------------- Add Easteregg
       if(!debug)
       {
         const eeLoader = new THREE.ObjectLoader();
@@ -270,10 +276,11 @@
         })
       }
 
+      // ----------------------- Add Asteroids to scene
       asteroids.forEach(p => groupClickable.add(p))
       scene.add(groupClickable)
 
-      // Sizes
+      // ----------------------- Register Resize
       const sizes = {
           width: window.innerWidth,
           height: window.innerHeight
@@ -294,6 +301,7 @@
           renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
       })
 
+      // ----------------------- Add Fullsize event
       document.addEventListener('dblclick', () => {
         var elem = document.querySelector('main')
         if (elem.requestFullscreen) {
@@ -312,24 +320,25 @@
         elem.style.height = '100%';      
       })
 
-      // Camera
+      // ----------------------- Add Camera
       const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1000, data.solarsystem_r * 2)
       scene.add(camera)
       camera.position.y = data.solarsystem_r / 2
 
-      // Add to Debug Gui
+      // ----------------------- Add to dat.gui
       var guiCamera = gui.addFolder('camera')
       guiCamera.open();
       guiCamera.add(camera.position, 'x').listen();
       guiCamera.add(camera.position, 'y').listen();
       guiCamera.add(camera.position, 'z').listen();   
-      var test = {'test': () => {
+      var test = {'showall': () => {
         clearInterval(timerHideableObjects)
         hideableObjects.forEach(o => o.visible = true)
 
       }} 
-      gui.add(test, "test")
+      gui.add(test, "showall")
 
+      // ----------------------- Load background texture
       const loader = new THREE.CubeTextureLoader();
       const texture = loader.load([
         // https://opengameart.org/content/space-skybox-0
@@ -342,14 +351,14 @@
       ]);
       scene.background = texture;
 
-      // Renderer
+      // ----------------------- Renderer
       const renderer = new THREE.WebGLRenderer({
           canvas: canvas
       })
       renderer.setSize(sizes.width, sizes.height)
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-      // Detect if mobile (motion sensor) or mouse
+      // ----------------------- Detect if mobile (motion sensor) or mouse
       if(isMobile(window.navigator).any)
       {
         controlAccelerator = new DeviceOrientationControls(camera)
@@ -366,7 +375,7 @@
       controlMouse.autoForward = false;
       controlMouse.dragToLook = false;
 
-      // Add Audio
+      // ----------------------- Add Audio
       const listener = new THREE.AudioListener();
       camera.add( listener );
       const sound = new THREE.PositionalAudio( listener );
@@ -377,9 +386,8 @@
         sound.setRefDistance( 6000000 );
       });    
 
-      // Animate
-      const clock = new THREE.Clock()
 
+      // ----------------------- Decide, which asteroids are far enough and hide them
       const addNearbies = (nearbyMeshes, max, type, results) => {
 
         // Get all nearby Object
@@ -420,10 +428,15 @@
         }
       }
 
+      
+      // ----------------------- Animate
+      const clock = new THREE.Clock()
+
       const tick = () =>
       {
           const delta = clock.getDelta();
           camera.updateWorldMatrix()
+
           // Rotate Planets
           planets.forEach(p => p.rotation.y += 0.02 * delta)
 
@@ -433,7 +446,7 @@
             console.log("Leaving Universe!")
           }
 
-          // Nearby
+          // Nearby check
           const localhuds = []
           var nearresultAsteroids = nearbyAsteroids.query(camera.position.x, camera.position.y, camera.position.z);
           var rearresultPlanets = nearbyPlanets.query(camera.position.x, camera.position.y, camera.position.z);
@@ -468,7 +481,7 @@
           window.requestAnimationFrame(tick)
       }
 
-      // Raycaster
+      // ----------------------- Raycaster
       const raycaster = new THREE.Raycaster();
       const pointer = new THREE.Vector2();
       canvas.addEventListener('click', e => {
@@ -526,6 +539,7 @@
 
       }, false);
 
+      // ----------------------- The rays from asteroids to planets
       function addConnections(active)
       {
         removeConnections()
@@ -552,7 +566,6 @@
 
       }
 
-
       // Create Composer
       const composer = new EffectComposer( renderer )
       composer.addPass(new RenderPass( scene, camera ))
@@ -573,7 +586,7 @@
 
       tick()
 
-      // Nearbychecker
+      // ----------------------- Nearbychecker
       timerHideableObjects = setInterval(() => {
 
         hideableObjects.forEach(o => o.visible = false)
@@ -589,6 +602,8 @@
         }      
 
       }, 500)
+
+      // Everything loaded
       loadingCounter--
     });
 
